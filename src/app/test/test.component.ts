cdb34a3f6a787ac60145bf116af2aec744a9f22f
Bikash Shah <bikashshah@Bikashs-MacBook-Air.local>
2022-02-04 18:48:34 +0545

    Intrerpolation Example

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+        <h1>
+          Hello {{name}}
+        </h1>
+        <!-- expression -->
+        <h2>{{2+3}}</h2>
+        <!-- //string concatenation -->
+        <h2> {{"Welcome again "+name}} </h2>
+        <h2>{{name.length}}</h2>
+        <h2>{{name.toUpperCase()}}</h2>
+        <h2>{{name.toLowerCase()}}</h2>
+        <h2>{{greetUser()}}</h2>
+        <h2>{{showage()}}</h2>
+        <h2>{{siteUrl}}</h2>
+         `,
+  styles: []
+
+})
+export class TestComponent implements OnInit {
+
+  //Interpolation Example
+  //assigning variable
+  public name = "Bikash Shah";
+  public age = 12;
+  public siteUrl = window.location.href
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+  //methods
+
+  greetUser() {
+    return "Hello " + this.name;
+  }
+  showage() {
+    return "Age: " + this.age;
+  }
+
+}
