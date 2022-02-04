84a2f5fb9b38afef01e0a8d36ce45022df16bfa8
Bikash Shah <bikashshah@Bikashs-MacBook-Air.local>
2022-02-04 19:21:51 +0545

    property binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
+  // template: `
+  //       <h1>
+  //         Hello {{name}}
+  //       </h1>
+  //       <!-- expression -->
+  //       <h2>{{2+3}}</h2>
+  //       <!-- //string concatenation -->
+  //       <h2> {{"Welcome again "+name}} </h2>
+  //       <h2>{{name.length}}</h2>
+  //       <h2>{{name.toUpperCase()}}</h2>
+  //       <h2>{{name.toLowerCase()}}</h2>
+  //       <h2>{{greetUser()}}</h2>
+  //       <h2>{{showage()}}</h2>
+  //       <h2>{{siteUrl}}</h2>
+  //        `,
   template: `
-        <h1>
-          Hello {{name}}
-        </h1>
-        <!-- expression -->
-        <h2>{{2+3}}</h2>
-        <!-- //string concatenation -->
-        <h2> {{"Welcome again "+name}} </h2>
-        <h2>{{name.length}}</h2>
-        <h2>{{name.toUpperCase()}}</h2>
-        <h2>{{name.toLowerCase()}}</h2>
-        <h2>{{greetUser()}}</h2>
-        <h2>{{showage()}}</h2>
-        <h2>{{siteUrl}}</h2>
-         `,
+  <h1>Hello {{name}}
+    <br>
+  <input [disabled]="isDabled" [id]="myId" type="text" value="bikas">
+  `,
   styles: []
 
 })
@@ -26,7 +31,11 @@ export class TestComponent implements OnInit {
   //assigning variable
   public name = "Bikash Shah";
   public age = 12;
-  public siteUrl = window.location.href
+  public siteUrl = window.location.href;
+  public isDabled = false;
+  
+  public myId ="testid";
+
   constructor() { }
 
 
